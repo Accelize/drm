30e79bc63140c090d23543ad65f8663dcad73543
jbleclere <jbleclere@accelize.com>
2019-02-13 14:20:49 +0000

    Refactor demo.py.

--- demo/demo.py
+++ demo/demo.py
@@ -20,19 +20,25 @@ driver_class = get_driver('aws_f1')
 driver = driver_class()
 
 # Instantiate
-drm_manager = accelize_drm.DrmManager(
-   './conf.json', './cred.json',
-   driver.read_register_callback,
-   driver.write_register_callback)
+err = 1
+try:
+    drm_manager = accelize_drm.DrmManager(
+       './conf.json', './cred.json',
+       driver.read_register_callback,
+       driver.write_register_callback)
 
-val = 987654321
-drm_manager.set(custom_field=val)
-print("Wrote custom field: ", val)
+    val = 987654321
+    drm_manager.set(custom_field=val)
+    print("Wrote custom field: ", val)
 
-val_back = drm_manager.get('custom_field')
-print("Read back custom field: ", val_back)
+    val_back = drm_manager.get('custom_field')
+    print("Read back custom field: ", val_back)
 
-if val_back != val:
-    print("Test failed")
-else:
-    print("Test passed")
+    if val_back == val:
+        err = 0
+
+finally:
+    if err:
+        print("Test failed")
+    else:
+        print("Test passed")
