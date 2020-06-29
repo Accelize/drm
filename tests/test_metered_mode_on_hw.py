80fb43643309a243d78b796a37ba06643e68a740
jbleclere <jbleclere@accelize.com>
2020-06-29 13:00:56 +0000

    Debug unittest

--- tests/test_metered_mode_on_hw.py
+++ tests/test_metered_mode_on_hw.py
@@ -676,10 +676,10 @@ def test_floating_limits(accelize_drm, conf_json, cred_json, async_handler):
     try:
         drm_manager0.activate()
         assert drm_manager0.get('license_status')
-        with pytest.raises(accelize_drm.exceptions.DRMWSTimedOut) as excinfo:
+        with pytest.raises(accelize_drm.exceptions.DRMWSError) as excinfo:
             drm_manager1.activate()
         assert search(r'Timeout on License request after .+ attempts', str(excinfo.value)) is not None
-        assert async_handler.get_error_code(str(excinfo.value)) == accelize_drm.exceptions.DRMWSTimedOut.error_code
+        assert async_handler.get_error_code(str(excinfo.value)) == accelize_drm.exceptions.DRMWSError.error_code
         async_cb1.assert_NoError()
     finally:
         drm_manager0.deactivate()
@@ -688,10 +688,10 @@ def test_floating_limits(accelize_drm, conf_json, cred_json, async_handler):
     try:
         drm_manager1.activate()
         assert drm_manager1.get('license_status')
-        with pytest.raises(accelize_drm.exceptions.DRMWSTimedOut) as excinfo:
+        with pytest.raises(accelize_drm.exceptions.DRMWSError) as excinfo:
             drm_manager0.activate()
         assert search(r'Timeout on License request after .+ attempts', str(excinfo.value)) is not None
-        assert async_handler.get_error_code(str(excinfo.value)) == accelize_drm.exceptions.DRMWSTimedOut.error_code
+        assert async_handler.get_error_code(str(excinfo.value)) == accelize_drm.exceptions.DRMWSError.error_code
         async_cb0.assert_NoError()
     finally:
         drm_manager1.deactivate()
