dd35f0509f10abf525e12b5566b60ba762502d4e
jbleclere <jbleclere@accelize.com>
2020-09-28 18:37:51 +0000

    Add test to LGDN unittest. Add packages marker to tests

--- source/drm_manager.cpp
+++ source/drm_manager.cpp
@@ -1768,7 +1768,6 @@ protected:
                         mExpirationTime = TClock::now() + std::chrono::seconds( mLicenseDuration );
                     }
                 }
-                Debug( "Released metering access mutex from licensing thread" );
 
             } catch( const Exception& e ) {
                 if ( e.getErrCode() != DRM_Exit ) {
