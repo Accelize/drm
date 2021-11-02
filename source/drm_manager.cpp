5f1cf64fd4330ceb158a02f9d898f3752fca7037
jbleclere <jbleclere@accelize.com>
2021-11-02 10:44:45 +0000

    Set DRM frequency to dummy value 100 for SW controller

--- source/drm_manager.cpp
+++ source/drm_manager.cpp
@@ -1288,8 +1288,12 @@ protected:
         json_request["saasChallenge"] = saasChallenge;
         json_request["meteringFile"]  = std::accumulate( meteringFile.begin(), meteringFile.end(), std::string("") );
         json_request["request"] = "open";
-        if ( !isConfigInNodeLock() && !mIsHybrid)
-            json_request["drm_frequency"] = mFrequencyCurr;
+        if ( !isConfigInNodeLock() ) {
+            if  ( mIsHybrid )
+                json_request["drm_frequency"] = 100;
+            else
+                json_request["drm_frequency"] = mFrequencyCurr;
+        }
         json_request["mode"] = (uint8_t)mLicenseType;
 
         return json_request;
