25263a25b3ba5261d0a0b7c34b26602e371ca299
jbleclere <jbleclere@accelize.com>
2021-11-23 10:11:11 +0000

    Remove traces for logging debug

--- source/provencore.cpp
+++ source/provencore.cpp
@@ -291,7 +291,6 @@ int pnc_session_request(pnc_session_t *session, uint32_t type, uint32_t flags)
     if (session == NULL || !session->configured || (flags & ~UINT32_C(0xffff)) != 0) {
         return -EINVAL;
     }
-    printf("pnc_session_t=%p, type=%d\n", session, type);
     return ioctl(session->fd, TZ_IOCTL(TZ_IOCTL_SEND, flags), type);
 }
 
