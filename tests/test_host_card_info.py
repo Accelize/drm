285a8c7d7e0d2984b4101c778e86fa16dfc262cd
jbleclere <jbleclere@accelize.com>
2022-07-13 07:30:16 +0000

    Update some tests

--- tests/test_host_card_info.py
+++ tests/test_host_card_info.py
@@ -8,7 +8,8 @@ from time import sleep
 from re import match
 
 
-def test_host_data_verbosity(accelize_drm, conf_json, cred_json, async_handler):
+def test_host_data_verbosity(accelize_drm, conf_json, cred_json, async_handler,
+                    log_file_factory):
     """
     Test all supported verbosity
     """
@@ -19,6 +20,8 @@ def test_host_data_verbosity(accelize_drm, conf_json, cred_json, async_handler):
     async_cb = async_handler.create()
     async_cb.reset()
     conf_json.reset()
+    logfile = log_file_factory.create(2)
+    conf_json['settings'].update(logfile.json)
 
     # Get full data
     conf_json['settings']['host_data_verbosity'] = 0
@@ -50,7 +53,7 @@ def test_host_data_verbosity(accelize_drm, conf_json, cred_json, async_handler):
         data_partial = drm_manager.get('host_data')
         assert type(data_partial) == dict
         assert len(str(data_partial))
-        assert len(str(data_full)) > len(str(data_partial)) > 0
+        assert len(str(data_full)) >= len(str(data_partial)) > 0
 
     # Get none data
     conf_json['settings']['host_data_verbosity'] = 2
@@ -95,16 +98,26 @@ def test_host_format(accelize_drm, conf_json, cred_json, async_handler,
     assert data.get('csp') is None
     host_card = data['host_card']
     assert host_card
-    assert host_card.get('runtime')
-    assert host_card.get('system')
-    board = host_card.get('board')
-    assert board
-    if not accelize_drm.is_ctrl_sw:
-        assert board.get('error')
-    assert board.get('xclbin')
-    info = board.get('info')
-    assert info
-    assert info.get('dsa_name') is not None
+    if host_card.get('xrt2'):
+        host_card = host_card['xrt2']
+        assert host_card['system']
+        host = host_card['system']['host']
+        assert host
+        assert host['os']
+        assert host['xrt']
+    else:
+        if host_card.get('xrt1'):
+            host_card = host_card['xrt1']
+        assert host_card.get('runtime')
+        assert host_card.get('system')
+        board = host_card.get('board')
+        assert board
+        if not accelize_drm.is_ctrl_sw:
+            assert board.get('error')
+        assert board.get('xclbin')
+        info = board.get('info')
+        assert info
+        assert info.get('dsa_name') is not None
     async_cb.assert_NoError()
 
 
