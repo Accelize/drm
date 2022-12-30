28fc4bb5202b6115771cfeb99b7499ba63ae2414
jbleclere <jbleclere@accelize.com>
2022-12-30 17:37:38 +0000

    Debug regression

--- tests/test_async_health.py
+++ tests/test_async_health.py
@@ -162,19 +162,25 @@ def test_health_period_modification(accelize_drm, conf_json, cred_json, async_ha
         drm_manager.activate()
         lic_duration = drm_manager.get('license_duration')
         wait_func_true(lambda: len(get_context()['data']) >= nb_health,
-                timeout=int(nb_health / (lic_duration % health_period)) + 1 )
+                timeout=int(nb_health / (lic_duration // health_period)) + 1 )
         drm_manager.deactivate()
     async_cb.assert_NoError()
     data_list = get_context()['data']
     assert len(data_list) >= nb_health
     wait_start = data_list.pop(0)[1]
+    delta_cnt = [0,0]
     for i, (start, end) in enumerate(data_list):
         delta = parser.parse(start) - parser.parse(wait_start)
-        if i < 2:
-            assert health_period <= int(delta.total_seconds()) <= health_period + 1
-        else:
+        delta_list.append(delta.total_seconds())
+        if delta.total_seconds() >= 2*health_period:
+            delta_cnt[1] += 1
             assert 2*health_period <= int(delta.total_seconds()) <= 2*health_period + 1
+        else:
+            delta_cnt[0] += 1
+            assert health_period <= int(delta.total_seconds()) <= health_period + 1
         wait_start = end
+    assert delta_cnt[0]
+    assert delta_cnt[1]
     assert get_proxy_error() is None
 
 
