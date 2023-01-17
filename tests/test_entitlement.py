309c4ea4a13ef8f5fc32bad834df4c45c72d3699
jbleclere <jbleclere@accelize.com>
2023-01-17 08:14:47 +0000

    Debug regression

--- tests/test_entitlement.py
+++ tests/test_entitlement.py
@@ -22,7 +22,6 @@ def test_entitlement_user1_metering(accelize_drm, conf_json, cred_json, async_ha
     logfile = log_file_factory.create(2)
     conf_json['settings'].update(logfile.json)
     conf_json.save()
-
     with accelize_drm.DrmManager(
             conf_json.path,
             cred_json.path,
@@ -62,27 +61,29 @@ def test_entitlement_user1_nodelock(accelize_drm, conf_json, cred_json, async_ha
     logfile = log_file_factory.create(2)
     conf_json['settings'].update(logfile.json)
     conf_json.save()
-
-    with accelize_drm.DrmManager(
-            conf_json.path,
-            cred_json.path,
-            driver.read_register_callback,
-            driver.write_register_callback,
-            async_cb.callback
-        ) as drm_manager:
-        assert drm_manager.get('license_type') == 'Node-Locked'
-        with pytest.raises(accelize_drm.exceptions.DRMWSReqError) as excinfo:
-            drm_manager.activate()
-        assert search(r'Accelize Web Service error 403', str(excinfo.value))
-        assert search(r"No valid entitlement available for this product", str(excinfo.value))
-        assert search(r"You can use this product by subscribing new entitlements", str(excinfo.value))
-        assert async_handler.get_error_code(str(excinfo.value)) == accelize_drm.exceptions.DRMWSReqError.error_code
-        async_cb.assert_Error(accelize_drm.exceptions.DRMWSReqError.error_code, "You can use this product by subscribing new entitlements")
-        async_cb.reset()
-    log_content = logfile.read()
-    assert search(r'No valid entitlement available for this product', log_content, IGNORECASE)
-    assert search(r'You can use this product by subscribing new entitlements', log_content, IGNORECASE)
-    logfile.remove()
+    try:
+        with accelize_drm.DrmManager(
+                conf_json.path,
+                cred_json.path,
+                driver.read_register_callback,
+                driver.write_register_callback,
+                async_cb.callback
+            ) as drm_manager:
+            assert drm_manager.get('license_type') == 'Node-Locked'
+            with pytest.raises(accelize_drm.exceptions.DRMWSReqError) as excinfo:
+                drm_manager.activate()
+            assert search(r'Accelize Web Service error 403', str(excinfo.value))
+            assert search(r"No valid entitlement available for this product", str(excinfo.value))
+            assert search(r"You can use this product by subscribing new entitlements", str(excinfo.value))
+            assert async_handler.get_error_code(str(excinfo.value)) == accelize_drm.exceptions.DRMWSReqError.error_code
+            async_cb.assert_Error(accelize_drm.exceptions.DRMWSReqError.error_code, "You can use this product by subscribing new entitlements")
+            async_cb.reset()
+        log_content = logfile.read()
+        assert search(r'No valid entitlement available for this product', log_content, IGNORECASE)
+        assert search(r'You can use this product by subscribing new entitlements', log_content, IGNORECASE)
+        logfile.remove()
+    finally:
+        driver.program_fpga()
 
 
 @pytest.mark.minimum
