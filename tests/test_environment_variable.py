b09153c38e9c72a311d3b829958c3743fe8e43a0
jbleclere <jbleclere@accelize.com>
2023-02-09 12:26:57 +0000

    Debug regression

--- tests/test_environment_variable.py
+++ tests/test_environment_variable.py
@@ -18,10 +18,10 @@ def test_env_var_DRMSAAS_URL(accelize_drm, conf_json, cred_json, async_handler,
     # Check when DRMSAAS_URL is set
     environ['DRMSAAS_URL'] = conf_json['licensing']['url']
     conf_json['licensing']['url'] = 'http://acme.com'
+    assert conf_json['licensing']['url'] != environ['DRMSAAS_URL']
     logfile = log_file_factory.create(1)
     conf_json['settings'].update(logfile.json)
     conf_json.save()
-    assert conf_json['licensing']['url'] != environ['DRMSAAS_URL']
 
     with accelize_drm.DrmManager(
             conf_json.path,
@@ -31,13 +31,14 @@ def test_env_var_DRMSAAS_URL(accelize_drm, conf_json, cred_json, async_handler,
             async_cb.callback
         ) as drm_manager:
         drm_manager.activate()
-        drm_manager.deactivate()
     async_cb.assert_NoError()
+    log_content = logfile.read()
+    assert search(r'Use environment variable DRMSAAS_URL: %s' % environ['DRMSAAS_URL'],
+                log_content, IGNORECASE)
 
     # Check when DRMSAAS_URL is unset
     del environ['DRMSAAS_URL']
     assert 'DRMSAAS_URL' not in  environ.keys()
-    cred_json.clear_cache()
 
     with accelize_drm.DrmManager(
             conf_json.path,
@@ -53,17 +54,21 @@ def test_env_var_DRMSAAS_URL(accelize_drm, conf_json, cred_json, async_handler,
     async_cb.assert_Error(accelize_drm.exceptions.DRMWSReqError.error_code, 'Accelize Web Service error 404')
     async_cb.reset()
     log_content = logfile.read()
-    assert search(r'Use environment variable DRMLIB_ENVVAR_URL', log_content, IGNORECASE)
     assert search(r'Accelize Web Service error 404 on HTTP request', log_content, IGNORECASE)
     logfile.remove()
 
 
-def test_env_var_DRMSAAS_CLIENT_ID(accelize_drm, conf_json, cred_json, async_handler):
+def test_env_var_DRMSAAS_CLIENT_ID(accelize_drm, conf_json, cred_json, async_handler,
+                                log_file_factory):
     """
     Test DRMSAAS_CLIENT_ID environment variable overwrite value in cred file
     """
     driver = accelize_drm.pytest_fpga_driver[0]
     async_cb = async_handler.create()
+
+    logfile = log_file_factory.create(1)
+    conf_json['settings'].update(logfile.json)
+    conf_json.save()
 
     # Check when DRMSAAS_CLIENT_ID is set
     environ['DRMSAAS_CLIENT_ID'] = cred_json['client_id']
@@ -80,6 +85,9 @@ def test_env_var_DRMSAAS_CLIENT_ID(accelize_drm, conf_json, cred_json, async_han
         ) as drm_manager:
         drm_manager.activate()
     async_cb.assert_NoError()
+    log_content = logfile.read()
+    assert search(r'Use environment variable DRMSAAS_CLIENT_ID: %s' % environ['DRMSAAS_CLIENT_ID'],
+                log_content, IGNORECASE)
 
     # Check when DRMSAAS_CLIENT_ID is unset
     del environ['DRMSAAS_CLIENT_ID']
@@ -87,27 +95,34 @@ def test_env_var_DRMSAAS_CLIENT_ID(accelize_drm, conf_json, cred_json, async_han
     cred_json.clear_cache()
 
     with pytest.raises(accelize_drm.exceptions.DRMWSReqError) as excinfo:
-        with accelize_drm.DrmManager(
-                conf_json.path,
-                cred_json.path,
-                driver.read_register_callback,
-                driver.write_register_callback,
-                async_cb.callback
-            ) as drm_manager:
-            pass
+        accelize_drm.DrmManager(
+            conf_json.path,
+            cred_json.path,
+            driver.read_register_callback,
+            driver.write_register_callback,
+            async_cb.callback
+        )
     assert "Accelize Web Service error 401" in str(excinfo.value)
     assert "invalid_client" in str(excinfo.value)
     assert accelize_drm.exceptions.DRMWSReqError.error_code in async_handler.get_error_code(str(excinfo.value))
     async_cb.assert_Error(accelize_drm.exceptions.DRMWSReqError.error_code, 'Accelize Web Service error 401')
     async_cb.reset()
+    log_content = logfile.read()
+    assert search(r'Accelize Web Service error 404 on HTTP request', log_content, IGNORECASE)
+    logfile.remove()
 
 
-def test_env_var_DRMSAAS_CLIENT_SECRET(accelize_drm, conf_json, cred_json, async_handler):
+def test_env_var_DRMSAAS_CLIENT_SECRET(accelize_drm, conf_json, cred_json, async_handler,
+                                log_file_factory):
     """
     Test DRMSAAS_CLIENT_SECRET environment variable overwrite value in cred file
     """
     driver = accelize_drm.pytest_fpga_driver[0]
     async_cb = async_handler.create()
+
+    logfile = log_file_factory.create(1)
+    conf_json['settings'].update(logfile.json)
+    conf_json.save()
 
     # Check when DRMSAAS_CLIENT_SECRET is set
     environ['DRMSAAS_CLIENT_SECRET'] = cred_json['client_secret']
@@ -123,8 +138,9 @@ def test_env_var_DRMSAAS_CLIENT_SECRET(accelize_drm, conf_json, cred_json, async
             async_cb.callback
         ) as drm_manager:
         drm_manager.activate()
-        drm_manager.deactivate()
     async_cb.assert_NoError()
+    log_content = logfile.read()
+    assert search(r'Use environment variable DRMSAAS_CLIENT_SECRET', log_content, IGNORECASE)
 
     # Check when DRMSAAS_CLIENT_SECRET is unset
     del environ['DRMSAAS_CLIENT_SECRET']
@@ -132,16 +148,18 @@ def test_env_var_DRMSAAS_CLIENT_SECRET(accelize_drm, conf_json, cred_json, async
     cred_json.clear_cache()
 
     with pytest.raises(accelize_drm.exceptions.DRMWSReqError) as excinfo:
-        with accelize_drm.DrmManager(
-                conf_json.path,
-                cred_json.path,
-                driver.read_register_callback,
-                driver.write_register_callback,
-                async_cb.callback
-            ) as drm_manager:
-            pass
+        accelize_drm.DrmManager(
+            conf_json.path,
+            cred_json.path,
+            driver.read_register_callback,
+            driver.write_register_callback,
+            async_cb.callback
+        )
     assert "Accelize Web Service error 401" in str(excinfo.value)
     assert "invalid_client" in str(excinfo.value)
     assert accelize_drm.exceptions.DRMWSReqError.error_code in async_handler.get_error_code(str(excinfo.value))
     async_cb.assert_Error(accelize_drm.exceptions.DRMWSReqError.error_code, 'Accelize Web Service error 401')
     async_cb.reset()
+    log_content = logfile.read()
+    assert search(r'Accelize Web Service error 404 on HTTP request', log_content, IGNORECASE)
+    logfile.remove()
